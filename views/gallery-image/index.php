978a854735c11cb94519b7b6ceb70712c1bcda18
SergeyMitkin <sergeymitkin@gmail.com>
2021-02-21 04:02:02 +0300

    Создано модальное окно для создания галереи

--- views/gallery-image/index.php
+++ views/gallery-image/index.php
@@ -2,51 +2,110 @@
 /* @var $this yii\web\View */
 
 use zxbodya\yii2\galleryManager\GalleryManager;
+use yii\bootstrap\Modal;
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model \app\models\tables\GalleryImage */
 ?>
 
-    <div id="div-gallery-create-button">
-        <button type="button" class="btn btn-success" id="gallery-create-button">Создать галерею</button>
-    </div>
+<?php
+// Модальное окно формы создания галереи
+Modal::begin([
+    'headerOptions' => [
+        'style' => 'display:none;'
+    ],
+    'toggleButton' => [
+        'label' => 'Создать галерею',
+        'tag' => 'button',
+        'id' => 'gallery-create-button',
+        'class' => 'btn btn-success'
+    ],
+    'footerOptions' => [
+        'style' => 'display:none;'
+    ],
+    'options' => [
+        'id' => 'create-gallery-modal'
+    ],
+]);
+
+    ActiveForm::begin([
+        'method' => 'post',
+        'options' => [
+            'class' => 'create-gallery-form',
+            'id' => 'create-gallery-form-id'
+        ]
+    ]);
+    echo '<div class="input-group input-group-sm form-group" id="group-for-gallery-name-input">' .
+    Html::label('Название галереи', 'gallery-modal-name-input').
+    Html::input(
+            'text',
+            'gallery_name',
+            '',
+            [
+                'id' => 'gallery-modal-name-input',
+                'class' => 'form-control',
+                'placeholder' => 'Название галереи'
+            ]
+    ) .
+    '</div>' .
+
+    '<div class="form-group">' .
+    Html::label('Описание галереи', 'gallery-modal-description-textarea') .
+    Html::textarea(
+            'gallery_description',
+            '',
+            [
+                'class' => 'form-control',
+                'id' => 'gallery-modal-description-textarea',
+                'placeholder' => 'Описание галереи',
+                'rows' => 3
+            ]
+    ) .
+    '</div>';
+    ActiveForm::end();
+Modal::end();
+?>
+
+<div class="row">
 
 <?
 for ($i=0; $i<count($galleries); $i++){
     $owner_id = $galleries[$i]['id'];
-
     ?>
-    <div class="row">
-        <div class="col-xs-12 text-center">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3><?=$galleries[$i]['name']?></h3>
-                    <p><?=$galleries[$i]['description']?></p>
-                </div>
-                <div class="panel-body">
-                    <?php
-                    if ($model[$i]->isNewRecord) {
-                        echo 'Can not upload images for new record';
-                    } else {
-                        echo GalleryManager::widget(
-                            [
-                                'model' => $model[$i],
-                                'behaviorName' => 'galleryBehavior',
-                                'apiRoute' => 'gallery-image/galleryApi'
-                            ]
-                        );
-                    }
-                    ?>
-                </div>
+    <div class="col-xs-12 text-center">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3><?=$galleries[$i]['name']?></h3>
+                <p><?=$galleries[$i]['description']?></p>
+            </div>
+            <div class="panel-body">
+                <?php
+                if ($model[$i]->isNewRecord) {
+                    echo 'Can not upload images for new record';
+                } else {
+                    echo GalleryManager::widget(
+                        [
+                            'model' => $model[$i],
+                            'behaviorName' => 'galleryBehavior',
+                            'apiRoute' => 'gallery-image/galleryApi'
+                        ]
+                    );
+                }
+                ?>
             </div>
         </div>
     </div>
-<?}
+    <?}
 ?>
-
+</div>
 <?php
 /*
 foreach ($model[0]->getBehavior('galleryBehavior')->getImages() as $image) {
     echo \yii\helpers\Html::img($image->getUrl('small'));
 }
 */
+
+// Регистрируем JS file
+$this->registerJSFile('@web/js/gallery-image-index.js',['depends' => [\yii\web\JqueryAsset::className()]]);
---
+++ web/js/gallery-image-index.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    //console.log('gallery-image');
+/*
+    $("#create-gallery-modal").on('show.bs.modal', function (event) {
+
+        var modal = $(this);
+
+        modal.find(".modal-body").html(
+            '<form class="create-gallery-form" id="create-gallery-form-id">' +
+
+                '<div class="group" id="group-for-gallery-name-input">' +
+                    '<label for="gallery-modal-name-input">Название галереи</label>' +
+                    '<span id="input-for-gallery-name">' +
+                        '<input type="text" id="gallery-modal-name-input" name="gallery_name" placeholder="Название галереи">' +
+                    '</span>' +
+                '</div>' +
+
+                '<div class="group" id="group-for-gallery-description-input">' +
+                    '<label for="gallery-modal-description-input">Описание галереи</label>' +
+                    '<span id="input-for-gallery-description">' +
+                        '<input type="text" id="gallery-modal-description-input" name="gallery_description" placeholder="Описание галереи">' +
+                    '</span>' +
+                '</div>' +
+
+                '<div class="group" align="center">' +
+                     '<button id="gallery-create-post-button" class="btn btn-success">Отправить</button>' +
+                '</div>' +
+
+            '</form>'
+        )
+        //console.log(modal);
+    })
+*/
+})
